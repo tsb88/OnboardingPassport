7b895502ae2048194d4fae9c3ff2bc83f3d352d7
YYG9ZZG <YYG9ZZG@ups.com>
2018-07-12 16:03:34 -0400

    merged branch tejas and anthony into master

--- OnboardingPassport/MainActivity.cs
+++ OnboardingPassport/MainActivity.cs
@@ -5,34 +5,32 @@ using Android.OS;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
+using System.IO;
+using SQLite;
+using Android.Content;
 
 namespace OnboardingPassport
 {
 	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+
+    
 	public class MainActivity : AppCompatActivity
 	{
-        ProgressBar pointsBar; //progress bar
-        ListView challengesList; //list of challenges
-        ListView leaderboardsList; //list of leaderboards
+        ProgressBar pointsBar;
+        ListView challengesList;
+        ListView leaderboardsList;
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 			SetContentView(Resource.Layout.activity_main);
 
-            pointsBar = FindViewById<ProgressBar>(Resource.Id.progressBar);
-            challengesList = FindViewById<ListView>(Resource.Id.listViewChallenges);
-            leaderboardsList = FindViewById<ListView>(Resource.Id.listViewLeaderboards);
-		}
+            Intent intent = new Intent(this, typeof(MainActivity));
+            string ADIDRecived = intent.GetStringExtra("ADID");
 
-        public void loadChallengesList()
-        {
-
+            var db = new SQLite.SQLiteConnection(dbPath);
+            db.CreateTable<MainActivityDataBase>();
+            var table = db.Table<MainActivityDataBase>();
         }
-
-        public void loadLeaderboardsList()
-        {
-
-        }
-	}
+    }
 }
 

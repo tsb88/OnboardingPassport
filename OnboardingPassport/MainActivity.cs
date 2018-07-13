52494966a5d829c526d0a663bc3aa9264f80bcf1
Galindo Anthony (WYV1JGF) <anthonygalindo@ups.com>
2018-07-12 21:40:32 -0400

    add listview. database, menu in Anthony branch

--- OnboardingPassport/MainActivity.cs
+++ OnboardingPassport/MainActivity.cs
@@ -16,11 +16,13 @@ namespace OnboardingPassport
     
 	public class MainActivity : AppCompatActivity
 	{
-        // This comment is from branch_Tejas
 
         ProgressBar pointsBar;
         ListView challengesList;
         ListView leaderboardsList;
+        Toolbar toolbar;
+        int Adder = 0;
+
         readonly string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "dbTest.db3");
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -28,12 +30,59 @@ namespace OnboardingPassport
 			base.OnCreate(savedInstanceState);
 			SetContentView(Resource.Layout.activity_main);
 
+            pointsBar = FindViewById<ProgressBar>(Resource.Id.progressBar);
+
+            challengesList = FindViewById<ListView>(Resource.Id.listViewChallenges);
+            leaderboardsList = FindViewById<ListView>(Resource.Id.listViewLeaderboards);
+
+
+            toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+
+            SetActionBar(toolbar);
+
+            ActionBar.Title = "Onboarding Passport";
+            toolbar.InflateMenu(Resource.Menu.menu_main);
+
             Intent intent = new Intent(this, typeof(MainActivity));
             string ADIDRecived = intent.GetStringExtra("ADID");
 
             var db = new SQLite.SQLiteConnection(dbPath);
             db.CreateTable<MainActivityDataBase>();
             var table = db.Table<MainActivityDataBase>();
+
+            var AddPoints = db.Query<MainActivityDataBase>("SELECT * FROM MainActivityDataBase WHERE ADID = ?", ADIDRecived);
+
+            foreach (var item in AddPoints)
+            {
+                Adder = item.Points + Adder;
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.settings:
+                    Intent intent = new Intent(this, typeof(Settings));
+                    StartActivity(intent);
+                    return true;
+                case Resource.Id.profile:
+                    Intent intent2 = new Intent(this, typeof(Profile));
+                    StartActivity(intent2);
+                    return true;
+                case Resource.Id.logout:
+                    Intent intent3 = new Intent(this, typeof(Logout));
+                    StartActivity(intent3);
+                    return true;
+                default:
+                    return true;
+            }
         }
     }
 }
